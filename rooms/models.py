30a802c719bc0e6db3cdb69c62f8edd4acd223aa
Léo Bernard <leo.bernard@etudiant.univ-reims.fr>
2024-04-16 16:14:00 +0200

    modification de la taille max du champ

--- rooms/models.py
+++ rooms/models.py
@@ -7,7 +7,7 @@ class RoomCategory(models.Model):
     nom_de_la_salle = models.CharField(max_length=50)
     description = models.TextField()
     nombre_de_personnes = models.IntegerField()
-    motif_de_reservation = models.CharField(max_length=50, default='')
+    motif_de_reservation = models.CharField(max_length=150, default='')
     image_de_la_salle = models.ImageField(
         upload_to='images/roomCategories',
         default='images/roomCategories/none.png'
