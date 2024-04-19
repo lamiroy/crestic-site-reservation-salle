7ca97774013701f010612cdc7e4ecc129c4c9671
Thomas PAZZE <thomas.pazze@etudiant.univ-reims.fr>
2024-04-19 13:02:10 +0200

    fix: Refactor admin.py to remove readonly_fields in BookRoomsAdmin

--- bookedrooms/admin.py
+++ bookedrooms/admin.py
@@ -6,8 +6,7 @@ from .models import BookedRoom
 class BookRoomsAdmin(admin.ModelAdmin):
     model = BookedRoom
     list_display = ['id', 'date', 'startTime', 'endTime', 'groups', 'status',
-                    'user', 'room_category', 'nbr_of_rooms', 'total_cost']
-    readonly_fields = ('total_cost',)
+                    'user', 'room_category', 'nbr_of_rooms']
 
 
 admin.site.register(BookedRoom, BookRoomsAdmin)
