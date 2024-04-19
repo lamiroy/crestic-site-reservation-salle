1c37bfbc5c5cf78023ef167d6d8206208fc0e682
Thomas PAZZE <thomas.pazze@etudiant.univ-reims.fr>
2024-04-19 12:34:38 +0200

    feat: Refactor admin.py to display new attributes in BookedRoom list view

--- bookedrooms/admin.py
+++ bookedrooms/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 from .models import BookedRoom
 
+
 class BookRoomsAdmin(admin.ModelAdmin):
     model = BookedRoom
-    list_display = ['id', 'start_date', 'end_date',
+    list_display = ['id', 'date', 'startTime', 'endTime', 'groups', 'status',
                     'user', 'room_category', 'nbr_of_rooms', 'total_cost']
-    readonly_fields = ('total_cost', )
+    readonly_fields = ('total_cost',)
 
 
 admin.site.register(BookedRoom, BookRoomsAdmin)
