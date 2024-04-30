352c8cf4d5e48dd50489fa9f99913fcfe3e4b877
bern0181 <leo.bernard@etudiant.univ-reims.fr>
2024-04-30 10:13:18 +0200

    edit: modification des urls pour inclure la page d'accueil

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RoomListView
+from .views import HomePageView, RoomListView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
     path('rooms/list/', RoomListView.as_view(), name='roomreservation_list')
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,9 @@ from django.views.generic import ListView
 
 from .models import RoomCategory
 
+class HomePageView(ListView):
+    model = RoomCategory
+    template_name = 'home.html'
 
 class RoomListView(ListView):
     model = RoomCategory
