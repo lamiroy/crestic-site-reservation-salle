f093a7bc42fe504cc0f6009331528ae0d69fbc54
bern0181 <leo.bernard@etudiant.univ-reims.fr>
2024-05-02 13:59:20 +0200

    edit: ajout de la page de profil et des routes et vues qui vont avec

--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, ListView
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class MyProfileView(ListView):
+    template_name = 'myprofile_view.html'
+    model = CustomUser
