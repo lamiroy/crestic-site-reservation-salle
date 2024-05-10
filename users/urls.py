9a59348b55b598c84ef48eee1741d53c44e1141e
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-05-10 13:13:50 +0200

    edit: base.html et users/urls.py
    Modification de l'accès à la page de profil et ajout de la déconnexion

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from .views import SignUpView, MyProfileView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('myprofile/', MyProfileView.as_view(), name='myprofile' )
+    path('signup/',
+         SignUpView.as_view(), name='signup'),
+    path('myprofile/',
+         MyProfileView.as_view(), name='myprofile')
 ]
