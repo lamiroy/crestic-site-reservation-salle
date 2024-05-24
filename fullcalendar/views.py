102f7053bb8123533618007b69e305c3cc96d4b2
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-05-24 21:59:40 +0200

    edit: fullcalendar
    Vérification de code, ajout de documentation manquante et ajout de la librairie pandas.

--- fullcalendar/views.py
+++ fullcalendar/views.py
@@ -1,12 +1,12 @@
-import os
-import pandas as pd
-from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
-from bookedrooms.models import BookedRoom
+import os  # Import du module os pour interagir avec le système d'exploitation
+import pandas as pd  # Import de la bibliothèque pandas pour la manipulation de données
+from django.http import HttpResponse  # Import de la classe HttpResponse pour créer des réponses HTTP
+from django.contrib.auth.decorators import user_passes_test  # Import du décorateur pour restreindre l'accès aux users
+from bookedrooms.models import BookedRoom  # Import du modèle BookedRoom pour les réservations de salles
 
 
-# Fonction de test pour vérifier si l'utilisateur est un administrateur
 def admin_required(user):
+    # Vérifie si l'utilisateur est un administrateur
     return user.is_superuser or user.isSecretary
 
 
@@ -24,6 +24,7 @@ def export_holiday_ics(request):
         # Renvoie le contenu .ics en réponse à la requête
         response = HttpResponse(ics_content, content_type='text/calendar')
         response['Content-Disposition'] = 'attachment; filename="calendarHoliday.ics"'
+
         return response
     else:
         # Génère une réponse 404 si le fichier n'existe pas
@@ -44,6 +45,7 @@ def export_bookedrooms_ics(request):
         # Renvoie le contenu .ics en réponse à la requête
         response = HttpResponse(ics_content, content_type='text/calendar')
         response['Content-Disposition'] = 'attachment; filename="calendarBookedroom.ics"'
+
         return response
     else:
         # Génère une réponse 404 si le fichier n'existe pas
@@ -63,7 +65,8 @@ def export_to_excel(request):
 
     # Récupérer les données de votre modèle
     data = BookedRoom.objects.select_related('user', 'room_category').all().values(
-        'id', 'date', 'startTime', 'endTime', 'groups', 'status', 'motif', 'peopleAmount', 'user__username', 'room_category__libRoom'  # Ajoutez les champs nécessaires ici
+        'id', 'date', 'startTime', 'endTime', 'groups', 'status', 'motif', 'peopleAmount', 'user__username',
+        'room_category__libRoom', 'room_category__id'
     )
 
     # Convertir les données en DataFrame pandas
@@ -78,7 +81,8 @@ def export_to_excel(request):
         'status': 'Statut actuel',
         'motif': 'Motif',
         'peopleAmount': 'Nombre de personnes',
-        'user__username': 'Réservant',
+        'user__username': 'Demandeur',
+        'room_category__id': 'Numéro de la salle',
         'room_category__libRoom': 'Nom de la salle'
     }
     df.rename(columns=column_mapping, inplace=True)
@@ -96,6 +100,7 @@ def export_to_excel(request):
         response = HttpResponse(excel_content, content_type='application/vnd.openxmlformats-officedocument'
                                                             '.spreadsheetml.sheet')
         response['Content-Disposition'] = 'attachment; filename="reservations.xlsx"'
+
         return response
     else:
         # Génère une réponse 404 si le fichier n'existe pas
