fcbf94ff18c37d37358fc4ace13a3b5f8109a349
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-05-24 21:23:29 +0200

    edit: dashboard
    Vérification de code et ajout de documentation manquante.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path  # Import de la fonction path pour définir les URL
 from .views import (
     RoomDashboardListView,  # Vue pour afficher la liste des salles
     RoomDashboardUpdateView,  # Vue pour mettre à jour les détails d'une salle
@@ -9,15 +9,19 @@ from .views import (
 )
 
 urlpatterns = [
-    # URLs pour les opérations sur les salles
+    # URL pour mettre à jour les détails d'une salle avec l'identifiant pk
     path('rooms/<int:pk>/edit/',
          RoomDashboardUpdateView.as_view(), name='roomdashboard_edit'),
+    # URL pour afficher les détails d'une salle avec l'identifiant pk
     path('rooms/<int:pk>/',
          RoomDashboardDetailView.as_view(), name='roomdashboard_detail'),
+    # URL pour supprimer une salle avec l'identifiant pk
     path('rooms/<int:pk>/delete/',
          RoomDashboardDeleteView.as_view(), name='roomdashboard_delete'),
+    # URL pour créer une nouvelle salle
     path('rooms/new/',
          RoomDashboardCreateView.as_view(), name='roomdashboard_new'),
+    # URL pour afficher la liste des salles
     path('rooms/',
          RoomDashboardListView.as_view(), name='roomdashboard_list'),
     # URL pour afficher la liste des réservations de salles
