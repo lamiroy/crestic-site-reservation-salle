b0da529a851d3b7cd5a89e8be1c3ba5b7399533c
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-05-24 22:08:03 +0200

    edit: RoomQueSTIC
    Vérification de code et ajout de documentation manquante.

--- RoomQueSTIC/wsgi.py
+++ RoomQueSTIC/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os  # Import du module os pour interagir avec le système d'exploitation
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # Import de la fonction pour obtenir l'application WSGI Django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_reservation_project.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application()  # Création de l'application WSGI pour le serveur web
