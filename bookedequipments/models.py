a75ddfc68e5529037f9722d0ab3b039213d085f4
bern0181 <leo.bernard@etudiant.univ-reims.fr>
2024-05-29 15:16:05 +0200

    modificiation des champs de bookedequipments

--- bookedequipments/models.py
+++ bookedequipments/models.py
@@ -8,18 +8,18 @@ from equipments.models import EquipmentCategory
 
 class BookedEquipment(models.Model):
     STATUS_CHOICES = [
-        ('pending', 'en attente'),
+        ('loaned', 'prêté'),
         ('canceled', 'annulé'),
-        ('validated', 'validé'),
+        ('back', 'rendu'),
+        ('late', 'en retard')
     ]
 
     date = models.DateField()
     startTime = models.TimeField()
     endTime = models.TimeField()
     groups = models.CharField(max_length=100)
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
     motif = models.CharField(max_length=100)
-    peopleAmount = models.IntegerField(default=1)
     user = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
@@ -29,17 +29,23 @@ class BookedEquipment(models.Model):
         on_delete=models.CASCADE,
     )
 
-    def clean(self):
-        # Your existing validation logic from form_valid() method
+    def perform_validations(self):
+        # Vérifiez que l'utilisateur existe avant de vérifier is_superuser
+        if self.user and (self.user.is_superuser or self.user.isSecretary):
+            return  # Ignorer toutes les validations si l'utilisateur est un superutilisateur
+
+        # Vérification de la validité de la date et de l'heure de début
         selected_date = self.date
-        if selected_date < date.today():
-            raise ValidationError('Vous ne pouvez pas changer la date pour une date antérieure à aujourd\'hui.')
+        if selected_date < date.today():  # Si la date est antérieure à aujourd'hui
+            raise ValidationError('Vous ne pouvez pas choisir une date antérieure à aujourd\'hui.')
 
         start_time = self.startTime
         if start_time:
-            if start_time < time(6, 0) or start_time > time(19, 30):
-                raise ValidationError('L\'heure de début doit être entre 6h00 et 19h30.')
+            # Vérification de l'heure de début dans les plages horaires autorisées
+            if start_time < time(8, 0) or start_time > time(18, 0):
+                raise ValidationError('L\'heure de début doit être entre 8h00 et 18h00.')
 
+            # Vérification de l'heure de début pour la même journée
             if selected_date == date.today():
                 current_time = datetime.now().time()
                 new_hour = current_time.hour + 1
@@ -53,17 +59,34 @@ class BookedEquipment(models.Model):
 
         end_time = self.endTime
         if end_time:
-            if end_time < time(6, 0) or end_time > time(22, 0):
-                raise ValidationError('L\'heure de fin doit être entre 6h00 et 22h00.')
-
+            # Vérification de l'heure de fin dans les plages horaires autorisées
+            if end_time < time(8, 0) or end_time > time(18, 0):
+                raise ValidationError('L\'heure de fin doit être entre 8h00 et 18h00.')
             if end_time <= start_time:
                 raise ValidationError('L\'heure de fin doit être supérieure à l\'heure de début.')
 
-        # For new bookings, the start date should not be less than today's date
+        # Ajout de la condition pour le samedi après 12h30 et le dimanche
+        if selected_date.weekday() == 5:  # Samedi (0 = lundi, 6 = dimanche)
+            if start_time >= time(12, 30):
+                raise ValidationError('Aucune réservation possible le samedi après 12h30.')
+        elif selected_date.weekday() == 6:  # Dimanche
+            raise ValidationError('Aucune réservation possible le dimanche.')
 
-        # For old bookings, you can't update the details 4 days before the start date
+        # Vérification qu'aucune réservation n'occupe la même salle pendant la même période
+        existing_bookings = BookedEquipment.objects.filter(
+            equipment_category=self.equipment_category,
+            date=self.date,
+            startTime__lt=self.endTime,
+            endTime__gt=self.startTime,
+        ).exclude(pk=self.pk)
 
-        # Loop through the start and end dates
+        # Filtrer les réservations existantes avec un statut autre que "pending"
+        existing_non_pending_bookings = existing_bookings.exclude(status='pending')
+
+        if existing_non_pending_bookings.exists():
+            raise ValidationError(
+                'Une réservation existante avec un statut autre que "pending" occupe déjà cet équipement pendant cette '
+                'période.')
 
     def get_absolute_url(self):
         return reverse('bookedequipments_detail', args=[str(self.id)])
