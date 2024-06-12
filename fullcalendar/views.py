6582eda73fd8210f896b2a2e911507f4ecff6b35
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2024-06-12 14:05:33 +0200

    ajout de la fonction de l'export equipment ics

--- fullcalendar/views.py
+++ fullcalendar/views.py
@@ -52,6 +52,27 @@ def export_bookedrooms_ics(request):
         return HttpResponse('Fichier .ics non trouvé', status=404)
 
 
+def export_bookedequipments_ics(request):
+    # Chemin absolu vers le fichier .ics existant
+    current_directory = os.path.dirname(__file__)
+    ics_file_path = os.path.join(current_directory, 'calendarFiles/calendarBookedequipments.ics')
+
+    # Vérifie si le fichier existe
+    if os.path.exists(ics_file_path):
+        # Lit le contenu du fichier .ics
+        with open(ics_file_path, 'rb') as f:
+            ics_content = f.read()
+
+        # Renvoie le contenu .ics en réponse à la requête
+        response = HttpResponse(ics_content, content_type='text/calendar')
+        response['Content-Disposition'] = 'attachment; filename="calendarBookedequipments.ics"'
+
+        return response
+    else:
+        # Génère une réponse 404 si le fichier n'existe pas
+        return HttpResponse('Fichier .ics non trouvé', status=404)
+
+
 @user_passes_test(admin_required)
 def export_to_excel(request):
     # Chemin absolu vers le dossier 'excel'
