e51514ce43539c384dd6a6448ec8f9dd252acae8
LAMIROY Bart <bart.lamiroy@loria.fr>
2024-08-23 09:38:00 +0200

    Ajustements pour mise en production

--- RoomQueSTIC/urls.py
+++ RoomQueSTIC/urls.py
@@ -44,4 +44,4 @@ urlpatterns = [
 
 if settings.DEBUG:  # Vérifie si le mode DEBUG est activé dans les paramètres de configuration
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
