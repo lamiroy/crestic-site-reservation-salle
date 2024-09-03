227b86d9e958b154dfd2e9cf3a7edd27be15ee40
LAMIROY Bart <bart.lamiroy@loria.fr>
2024-09-03 13:15:40 +0200

    Getting signals working

--- bookedrooms/signals.py
+++ bookedrooms/signals.py
@@ -1,4 +1,5 @@
-from django.db.models.signals import post_save, pre_delete
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
 from bookedrooms.models import BookedRoom
@@ -10,7 +11,16 @@ def send_email(sender, instance, created=False, **kwargs):
     if created:
         print('Creation')
 
+    print(instance.status)
+    print(instance.user)
+    user = User.objects.get(pk=instance.user)
+    print(user.email)
+
 
 @receiver(pre_delete, sender=BookedRoom)
 def send_email(sender, instance, **kwargs):
     print(f'pre_delete trigger for {instance.pk}')
+
+@receiver(post_delete, sender=BookedRoom)
+def send_email(sender, instance, **kwargs):
+    print(f'post_delete trigger for {instance.pk}')
