66331eee14997e09e3515098822ea3fefcc2e88a
LAMIROY Bart <bart.lamiroy@loria.fr>
2024-09-03 21:05:00 +0200

    Cleaning up email sending

--- RoomQueSTIC/utils.py
+++ RoomQueSTIC/utils.py
@@ -1,6 +1,21 @@
 from django.core.mail import send_mail  # Importe la fonctionnalité d'envoi de courrier électronique Django
 from RoomQueSTIC import settings  # Importe les paramètres de configuration de l'application
 
+def reservation_url(booked_room) -> str:
+    return f'https://crestic.univ-reims.fr/reservations/roombooking/{booked_room.id}/edit/'
+
+def greeting_template(booked_room) -> str:
+    return f'Bonjour {booked_room.user.first_name},'
+
+def text_template(booked_room) -> str:
+    text = f'Salle: {booked_room.room_category}\n' \
+              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
+              f'Date: {booked_room.date}\n' \
+              f'Heure de début: {booked_room.startTime}\n' \
+              f'Heure de fin: {booked_room.endTime}\n' \
+              f'Groupe/Laboratoire: {booked_room.groups}\n' \
+              f'Motif: {booked_room.motif}\n'
+    return text
 
 # CREATION DE RESERVATION
 def send_reservation_confirmation_email_admin(booked_room):
@@ -8,17 +23,11 @@ def send_reservation_confirmation_email_admin(booked_room):
     Envoie un mail qui confirme directement la réservation faite par l'admin
     """
     subject = 'Votre réservation a bien été validée'
-    message = f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n'
+    message = text_template(booked_room) + '\n\n' + 'https://crestic.univ-reims.fr/reservations'
+
     sender = settings.DEFAULT_FROM_EMAIL
     recipient_list = [booked_room.user.email] + settings.EMAIL_RECIPIENTS
-    code = send_mail(subject, message, sender, recipient_list, fail_silently=False)
-    print(f'email return code = {code}')
+    send_mail(subject, message, sender, recipient_list, fail_silently=False)
 
 
 def send_request_validation_email_admin(booked_room):
@@ -30,144 +39,19 @@ def send_request_validation_email_admin(booked_room):
               f' {booked_room.user.last_name} ({booked_room.user.email}).\n' \
               f'Salle: {booked_room.room_category}\n' \
               f'Motif: {booked_room.motif}\n' \
-              f'Veuillez valider ou refuser cette demande de réservation sur https://crestic.univ-reims.fr/reservations.'
+              f'Veuillez valider ou refuser cette demande de réservation sur {reservation_url(booked_room)}.'
     sender = settings.DEFAULT_FROM_EMAIL
     recipient_list = settings.EMAIL_RECIPIENTS
-    code = send_mail(subject, message, sender, recipient_list)
-    print(f'email return code = {code}')
+    send_mail(subject, message, sender, recipient_list)
 
 def send_reservation_acknowledgement_email_user(booked_room):
     """
     Envoie un mail qui atteste de la demande réservation d'un utilisateur
     """
-    subject = 'Demande de réservation a bien été effectuée'
-    message = f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'Veuillez patienter pendant qu''une secrétaire valide ou refuse cette demande de réservation.'
+    subject = 'Votre demande de réservation a été effectuée'
+    message = greeting_template(booked_room) + '\n\n' + text_template(booked_room) + f"\n\nVotre demande est en attente de validation.\n\n" + reservation_url(booked_room)
     sender = settings.DEFAULT_FROM_EMAIL
     recipient_list = [booked_room.user.email]
-    code = send_mail(subject, message, sender, recipient_list)
-    print(f'email return code = {code}')
-
-# MODIFICATION DE RESERVATION
-def send_reservation_update_email_admin(booked_room):
-    """
-    Envoie un mail qui montre le changement d'une réservation faite par l'admin
-    """
-    subject = 'Réservation modifiée'
-    message = f'La réservation suivante a été modifiée.\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'La réservation a été modifiée avec succès.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
-
-
-def send_reservation_update_email_user_by_admin(booked_room):
-    """
-    Envoie un mail qui montre le changement d'une réservation par l'admin faite par un utilisateur
-    """
-    subject = 'Mise à jour de votre réservation'
-    message = f'La réservation suivante a été modifiée par les secrétaires:\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'Veuillez patienter pendant qu''une secrétaire valide ou refuse cette modification.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
-
-
-def send_reservation_update_email_user(booked_room):
-    """
-    Envoie un mail qui indique à l'utilisateur que sa demande de réservation doit être acceptée à nouveau
-    """
-    subject = 'Mise à jour de votre réservation'
-    message = f'La réservation suivante a été modifiée:\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'Veuillez patienter pendant qu''une secrétaire valide ou refuse cette modification.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
-
-
-def send_reservation_update_email_admin_alert(booked_room):
-    """
-    Envoie un mail qui indique à l'admin qu'un utilisateur a modifié sa réservation
-    """
-    subject = 'Mise à jour d''une réservation'
-    message = f'La réservation suivante a été modifiée par {booked_room.user.first_name}' \
-              f' {booked_room.user.last_name}.\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'Veuillez valider ou refuser cette modification.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
-
-
-# ANNULATION DE RESERVATION
-def send_reservation_cancellation_email_user_alert_validated_pending_reservation(booked_room):
-    """
-    Envoie un mail qui indique à l'utilisateur que sa réservation a été annulée
-    """
-    subject = 'Réservation annulée'
-    message = f'Votre réservation suivante a été annulée.\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'La réservation a été annulée.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
-
-
-def send_reservation_cancellation_email_admin_alert_validated_pending_reservation(booked_room):
-    """
-    Envoie un mail qui indique à l'admin qu'une réservation a été annulée
-    """
-    subject = 'Réservation annulée'
-    message = f'Votre réservation suivante a été annulée.\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'La réservation a été annulée.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
     send_mail(subject, message, sender, recipient_list)
 
 
@@ -183,47 +67,8 @@ def send_reservation_cancellation_email_user(booked_room):
               f'Heure de début: {booked_room.startTime}\n' \
               f'Heure de fin: {booked_room.endTime}\n' \
               f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'La réservation a été annulée.\n'
+              f'Motif: {booked_room.motif}\n'
     sender = settings.DEFAULT_FROM_EMAIL
     recipient_list = [booked_room.user.email]
     send_mail(subject, message, sender, recipient_list)
 
-
-def send_reservation_cancellation_email_admin_alert_validated_reservation(booked_room):
-    """
-    Envoie un mail qui confirme l'annulation effectuée par l'admin d'une réservation (confirmée)
-    """
-    subject = 'Réservation annulée'
-    message = f'La réservation (confirmée) suivante a été annulée par {booked_room.user.first_name}' \
-              f' {booked_room.user.last_name}.\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'La réservation a été annulée.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
-
-
-def send_reservation_cancellation_email_user_alert_validated_reservation(booked_room):
-    """
-    Envoie un mail qui confirme l'annulation effectuée par l'utilisateur d'une réservation (confirmée)
-    """
-    subject = 'Réservation annulée'
-    message = f'Votre réservation (confirmée) suivante a été annulée:\n' \
-              f'Salle: {booked_room.room_category}\n' \
-              f'Nombre de personnes: {booked_room.peopleAmount}\n' \
-              f'Date: {booked_room.date}\n' \
-              f'Heure de début: {booked_room.startTime}\n' \
-              f'Heure de fin: {booked_room.endTime}\n' \
-              f'Groupe/Laboratoire: {booked_room.groups}\n' \
-              f'Motif: {booked_room.motif}\n' \
-              f'La réservation a été annulée.'
-    sender = settings.DEFAULT_FROM_EMAIL
-    recipient_list = settings.EMAIL_RECIPIENTS
-    send_mail(subject, message, sender, recipient_list)
