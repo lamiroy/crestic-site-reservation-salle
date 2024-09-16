689903c6f75937033c4e4c02bd0a89481c8e2077
LAMIROY Bart <bart.lamiroy@loria.fr>
2024-09-16 10:25:30 +0200

    another one bites the dust

--- RoomQueSTIC/utils.py
+++ RoomQueSTIC/utils.py
@@ -83,7 +83,7 @@ class MessageTemplate:
 class RoomReservationTemplate(MessageTemplate):
 
     def __init__(self, booking):
-        self.super().__init__(booking)
+        super().__init__(booking)
         self._booking_type_name = 'Salle'
 
     @property
@@ -96,7 +96,7 @@ class RoomReservationTemplate(MessageTemplate):
 class EquipmentReservationTemplate(MessageTemplate):
 
     def __init__(self, booking):
-        self.super().__init__(booking)
+        super().__init__(booking)
         self._booking_type_name = 'Équipement'
 
     @property
