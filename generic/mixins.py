d8a57449ab2d58481bdab99a0ee08f88c067bb69
LAMIROY Bart <bart.lamiroy@loria.fr>
2024-09-20 16:37:55 +0200

    Corrected overly prudent modifications ?

--- generic/mixins.py
+++ generic/mixins.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from django.views.generic.detail import SingleObjectMixin
 
 
-class UserIsOwnerOrAdminMixin(TemplateView, SingleObjectMixin):
+class UserIsOwnerOrAdminMixin:
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
         if not (request.user == obj.user or request.user.is_superuser or request.user.isSecretary):
